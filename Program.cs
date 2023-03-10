8ef6aa9f388b160fb30965fbeda8981307f977e5
Nikolay Borovik <nik.Borovik@bk.ru>
2023-03-10 14:39:06 +1000

    1st part of the method added which generates a temporary array of the defined length strings

--- Program.cs
+++ Program.cs
@@ -3,6 +3,19 @@ Console.Clear();
 
 string[] text = { "Whether", "the", "weather", "is", "fine", ",", "Whether the weather", "is", "not" };
 
-
+string[] ToStringWithMax3Chars(string[] str)
+{
+    int count = 0;
+    string[] temp = new string[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        char[] a = text[i].ToCharArray();
+        if (a.Length <= 3)
+        {
+            temp[count] = text[i];
+            count++;
+        }
+    }
+}
 
 
